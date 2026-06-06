0bf83b46444e58ae1b0d404f7acaa23bb7453aa7
TSingsNing <1.17154712e+08+TSingsNing@users.noreply.github.com>
2026-06-06 08:49:59 +0000

    fix: refresh leaderboard page for device debug

--- pages/leaderboard/leaderboard.js
+++ pages/leaderboard/leaderboard.js
@@ -1,7 +1,19 @@
 const { getLeaderboard } = require('../../utils/rank.js')
 
 Page({
-  data: { list: [] },
-  onShow() { this.setData({ list: getLeaderboard() }) },
-  backHome() { wx.reLaunch({ url: '/pages/index/index' }) }
+  data: {
+    list: []
+  },
+  onLoad() {
+    this.refreshLeaderboard()
+  },
+  onShow() {
+    this.refreshLeaderboard()
+  },
+  refreshLeaderboard() {
+    this.setData({ list: getLeaderboard() })
+  },
+  backHome() {
+    wx.reLaunch({ url: '/pages/index/index' })
+  }
 })
